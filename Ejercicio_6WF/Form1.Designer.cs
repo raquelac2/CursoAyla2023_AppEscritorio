c9f953a9ee171d4c4237463e60cfdae72c39f192
raquelac2 <raquelac2@hotmail.com>
2023-05-13 17:50:47 +0200

    Ejercicio 6WF: Buscador de poblaciones sin terminar

---
+++ Ejercicio_6WF/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Ejercicio_6WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            btnEntrar = new Button();
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // btnEntrar
+            // 
+            btnEntrar.Font = new Font("Tahoma", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            btnEntrar.ForeColor = Color.DarkCyan;
+            btnEntrar.Location = new Point(125, 244);
+            btnEntrar.Name = "btnEntrar";
+            btnEntrar.Size = new Size(202, 91);
+            btnEntrar.TabIndex = 0;
+            btnEntrar.Text = "Entrar";
+            btnEntrar.UseVisualStyleBackColor = true;
+            btnEntrar.Click += btnEntrar_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(125, 12);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(202, 148);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Tahoma", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.DarkCyan;
+            label1.Location = new Point(51, 163);
+            label1.Name = "label1";
+            label1.Size = new Size(350, 23);
+            label1.TabIndex = 2;
+            label1.Text = "Buscador de poblaciones de España";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Tahoma", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            label2.ForeColor = Color.MediumTurquoise;
+            label2.Location = new Point(188, 198);
+            label2.Name = "label2";
+            label2.Size = new Size(76, 14);
+            label2.TabIndex = 3;
+            label2.Text = "Version 1.0";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightCyan;
+            ClientSize = new Size(459, 347);
+            ControlBox = false;
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(btnEntrar);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnEntrar;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ Ejercicio_6WF/FormPrincipal.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Ejercicio_6WF
+{
+    partial class FormPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblComunidad = new Label();
+            cboComunidad = new ComboBox();
+            lblProvincia = new Label();
+            cboProvincia = new ComboBox();
+            SuspendLayout();
+            // 
+            // lblComunidad
+            // 
+            lblComunidad.AutoSize = true;
+            lblComunidad.Location = new Point(12, 9);
+            lblComunidad.Name = "lblComunidad";
+            lblComunidad.Size = new Size(76, 15);
+            lblComunidad.TabIndex = 0;
+            lblComunidad.Text = "Comunidad: ";
+            // 
+            // cboComunidad
+            // 
+            cboComunidad.FormattingEnabled = true;
+            cboComunidad.Location = new Point(94, 9);
+            cboComunidad.Name = "cboComunidad";
+            cboComunidad.Size = new Size(168, 23);
+            cboComunidad.TabIndex = 1;
+            // 
+            // lblProvincia
+            // 
+            lblProvincia.AutoSize = true;
+            lblProvincia.Location = new Point(293, 9);
+            lblProvincia.Name = "lblProvincia";
+            lblProvincia.Size = new Size(62, 15);
+            lblProvincia.TabIndex = 2;
+            lblProvincia.Text = "Provincia: ";
+            // 
+            // cboProvincia
+            // 
+            cboProvincia.FormattingEnabled = true;
+            cboProvincia.Location = new Point(366, 9);
+            cboProvincia.Name = "cboProvincia";
+            cboProvincia.Size = new Size(184, 23);
+            cboProvincia.TabIndex = 3;
+            // 
+            // FormPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.LightCyan;
+            ClientSize = new Size(588, 398);
+            Controls.Add(cboProvincia);
+            Controls.Add(lblProvincia);
+            Controls.Add(cboComunidad);
+            Controls.Add(lblComunidad);
+            Name = "FormPrincipal";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "FormPrincipal";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblComunidad;
+        private ComboBox cboComunidad;
+        private Label lblProvincia;
+        private ComboBox cboProvincia;
+    }
+}
